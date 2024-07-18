d2f6775ffc5b891a0c92ebbf0a6cded681b52ebe
JoeDB993 <131511247+JoeDB993@users.noreply.github.com>
2024-07-17 23:26:16 -0600

    Agregué los 2 últimos super héroes

--- EjercicioClase/Program.cs
+++ EjercicioClase/Program.cs
@@ -12,3 +12,23 @@ superheroe.SuperPoder.descripcion = "Es capaz de levantar objetos extremadamente
 superheroe.SuperPoder.nivel = "Indestructible.";
 superheroe.Imprimir();
 
+
+SuperHeroe superheroe2 = new SuperHeroe();
+superheroe2.nombre = "Ana";
+superheroe2.identidadsecreta = "Invisible Woman";
+superheroe2.ciudad = "New York";
+superheroe2.puedevolar = false;
+superheroe2.SuperPoder.NombrePoder = "Invisibilidad.";
+superheroe2.SuperPoder.descripcion = "Puede volverse invisible a voluntad.";
+superheroe2.SuperPoder.nivel = "Avanzado.";
+superheroe2.Imprimir();
+
+SuperHeroe superheroe3 = new SuperHeroe();
+superheroe3.nombre = "Carlos";
+superheroe3.identidadsecreta = "Flash";
+superheroe3.ciudad = "Central City";
+superheroe3.puedevolar = false;
+superheroe3.SuperPoder.NombrePoder = "Super velocidad.";
+superheroe3.SuperPoder.descripcion = "Es capaz de moverse a velocidades increíbles, superando la velocidad del sonido.";
+superheroe3.SuperPoder.nivel = "Máximo.";
+superheroe3.Imprimir();
